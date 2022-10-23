bf507a9e7349ddfc7cfdbc3e4bda8a4aed47804a
anugoyal998 <anugoyal998@gmail.com>
2022-10-23 20:45:43 +0530

    cors

--- server/server.js
+++ server/server.js
@@ -15,7 +15,7 @@ const io = new Server(server, {
   },
 });
 
-app.use(cors({ origin: ["http://localhost:3000","http://127.0.0.1:5173/", process.env.CLIENT] }));
+app.use(cors())
 app.use(express.json());
 app.use("/", router);
 app.get("/", (req, res) => {
